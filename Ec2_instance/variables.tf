7605b93b986f648cd8e281b44ac3dbafba542754
Vignesh8312 <vignesh.murugan1312@gmail.com>
2025-07-11 20:21:42 +0530

    Added dynamic tags support to EC2 module

--- Ec2_instance/variables.tf
+++ Ec2_instance/variables.tf
@@ -12,3 +12,8 @@ variable "ami_id" {
   description = "AMI ID to launch EC2"
   type        = string
 }
+variable "tags" {
+  description = "Tags to apply to the EC2 instance"
+  type        = map(string)
+  default     = {}
+}
