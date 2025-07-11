7e71b1a0fdb66a0d5aa0e6865c48fedbb540f631
Vignesh8312 <vignesh.murugan1312@gmail.com>
2025-07-11 20:08:47 +0530

    first commit

---
+++ S3/main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+
+resource "aws_s3_bucket" "my_s3" {
+  bucket = var.bucket_name
+  tags   = var.tags
+}
+
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
