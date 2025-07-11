58996cd0139d53dfcd16ad2520d5a93fa55e5cbf
Vignesh8312 <vignesh.murugan1312@gmail.com>
2025-07-11 20:49:40 +0530

    Enhance main.tf and variable.tf

--- Ec2_instance/main.tf
+++ Ec2_instance/main.tf
@@ -5,8 +5,6 @@ provider "aws" {
 resource "aws_instance" "my_ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
-
-  tags = {
-    Name = "Vignesh-EC2"
-  }
+  tags          = var.tags
 }
+
--- Ec2_instance/variables.tf
+++ Ec2_instance/variables.tf
@@ -1,8 +1,3 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-}
-
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
@@ -12,6 +7,12 @@ variable "ami_id" {
   description = "AMI ID to launch EC2"
   type        = string
 }
+
+variable "aws_region" {
+  description = "AWS region to launch resources"
+  type        = string
+}
+
 variable "tags" {
   description = "Tags to apply to the EC2 instance"
   type        = map(string)
